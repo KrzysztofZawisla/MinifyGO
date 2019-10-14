0aa22749036cbc7fb8bdb549b3649fe59b112eb9
KrzysztofZawisla <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-10-14 18:45:46 +0200

    Beautify code

--- src/main.go
+++ src/main.go
@@ -12,56 +12,43 @@ import (
 
 var outputToTheSameFile = flag.Bool("outputToTheSameFile", false, "When you set this flag to the true program will save minified code to the same file. When you set this flag to the false program will create a new file with extension .min.js next to the origin file.")
 
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
 func main() {
 	flag.Parse()
 	_, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	checkErr(err)
 	minifyJSON, err := ioutil.ReadFile("minify.json")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	checkErr(err)
 	var minifyJSONStruct map[string]interface{}
 	err = json.Unmarshal(minifyJSON, &minifyJSONStruct)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	checkErr(err)
 	for key := range minifyJSONStruct {
 		if key == "files" {
 			for _, value := range minifyJSONStruct[key].([]interface{}) {
 				cmd := exec.Command("minify", value.(string))
 				fileName := strings.Split(value.(string), ".")
 				outFile, err := os.Create(fileName[0] + ".min.js")
-				if err != nil {
-					log.Fatal(err.Error())
-				}
+				checkErr(err)
 				defer outFile.Close()
 				cmd.Stdout = outFile
 				err = cmd.Start()
-				if err != nil {
-					log.Fatal(err.Error())
-				}
+				checkErr(err)
 				cmd.Wait()
 				if *outputToTheSameFile {
-					if err != nil {
-						log.Fatal(err.Error())
-					}
 					minifyText, err := ioutil.ReadFile(fileName[0] + ".min.js")
-					if err != nil {
-						log.Fatal(err.Error())
-					}
+					checkErr(err)
 					minifyTextAsString := strings.TrimSpace(string(minifyText))
 					minifyText = []byte(minifyTextAsString)
 					err = ioutil.WriteFile(value.(string), minifyText, 0644)
-					if err != nil {
-						log.Fatal(err.Error())
-					}
+					checkErr(err)
 					outFile.Close()
 					err = os.Remove(fileName[0] + ".min.js")
-					if err != nil {
-						log.Fatal(err.Error())
-					}
+					checkErr(err)
 				}
 			}
 		}
